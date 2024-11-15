76878babd4865fcba9f297f07401da6b9d1e8256
junior <ing.giraldo02@gmail.com>
2024-11-15 15:12:22 +0100

    modifique el codigo inicial y el nombre del csv

--- data/codcom.py
+++ data/codcom.py
@@ -1,14 +1,18 @@
-import matplotlib.pyplot as plt
+import charts
+import read_csv
+import utils
 
+def run(): 
+  data = read_csv.read_csv('./app/data.csv')
 
-def generate_bar_chart(labels, values): 
-  fig, ax = plt.subplots() 
-  ax.bar(labels, values)
-  plt.show()
+  country = input('Type Country => ')
 
-def generate_pie_chart(labels, values):
-  fig, ax = plt.subplots()
-  ax.pie(values, labels=labels)
-  ax.axis('equal')
-  plt.show()
+  result = utils.population_by_country(data, country)
 
+  if len(result) > 0:
+    country = result[0]
+    labels, values = utils.get_population(country)
+    charts.generate_bar_chart(labels, values)
+
+if __name__ == '__main__':
+  run() 
